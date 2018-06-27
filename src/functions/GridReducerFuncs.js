a97ce66d4fd799e02df1ea5f3f669fff38b36397
Ivan Opalchuk <alopal42@gmail.com>
2018-06-27 14:39:52 +0300

    Speshimen

--- src/functions/GridReducerFuncs.js
+++ src/functions/GridReducerFuncs.js
@@ -19,36 +19,36 @@ export function addToContacts(user){
 }
 
 export function searchForFunc (val){
-    var tempArr = dataJSON.users;
-    store.dispatch(onCrossClick());
-    for (var i = 0; i < dataJSON.users.length; i++) {
-        if (tempArr[i].name.toLowerCase().indexOf(val.toLowerCase()) > -1) {
-            tempArr[i].name = tempArr[i].name.toUpperCase();
-        } else if (tempArr[i].surname.toLowerCase().indexOf(val.toLowerCase()) > -1) {
-            tempArr[i].surname = tempArr[i].surname.toUpperCase();
-        } else if (tempArr[i].city.toLowerCase().indexOf(val.toLowerCase()) > -1) {
-            tempArr[i].city = tempArr[i].city.toUpperCase();
-        }
-    }
-    if(val === ''){store.dispatch(onCrossClick());}
     return function (dispatch){
+        var tempArr = dataJSON.users;
+        store.dispatch(onCrossClick());
+        for (var i = 0; i < dataJSON.users.length; i++) {
+            if (tempArr[i].name.toLowerCase().indexOf(val.toLowerCase()) > -1) {
+                tempArr[i].name = tempArr[i].name.toUpperCase();
+            } else if (tempArr[i].surname.toLowerCase().indexOf(val.toLowerCase()) > -1) {
+                tempArr[i].surname = tempArr[i].surname.toUpperCase();
+            } else if (tempArr[i].city.toLowerCase().indexOf(val.toLowerCase()) > -1) {
+                tempArr[i].city = tempArr[i].city.toUpperCase();
+            }
+        }
+        if(val === ''){store.dispatch(onCrossClick());}
         dispatch(setUsers(tempArr));
     }
 }
 
 export function onCrossClick() {
-    var capitalizeFunc = function (string){
-        string = string.toLowerCase();
-        string = string.charAt(0).toUpperCase() + string.slice(1);
-        return(string);
-    };
-    var tempArr = store.getState().GridReducer.users;
-    for (var i = 0; i < tempArr.length-1; i++) {
-        tempArr[i].name = capitalizeFunc(tempArr[i].name);
-        tempArr[i].city = capitalizeFunc(tempArr[i].city);
-        tempArr[i].surname = capitalizeFunc(tempArr[i].surname);
-    }
     return function (dispatch){
+        var capitalizeFunc = function (string){
+            string = string.toLowerCase();
+            string = string.charAt(0).toUpperCase() + string.slice(1);
+            return(string);
+        };
+        var tempArr = store.getState().GridReducer.users;
+        for (var i = 0; i < tempArr.length-1; i++) {
+            tempArr[i].name = capitalizeFunc(tempArr[i].name);
+            tempArr[i].city = capitalizeFunc(tempArr[i].city);
+            tempArr[i].surname = capitalizeFunc(tempArr[i].surname);
+        }
         dispatch(setUsers(tempArr));
     }
 }
